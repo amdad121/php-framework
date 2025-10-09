6098dd7a50889d9f1f43782e04f08c809d7d0e97
Amdadul Haq <amdadulhaq781@gmail.com>
2025-10-09 18:03:58 +0600

    initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+$app = new \Amdadulhaq\PhpFramework\Application();
+$app->run();
